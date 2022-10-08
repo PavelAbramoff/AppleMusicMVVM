52b793758e375a977508ffc1d7039d4689b5f22c
Pavel <Pavel.818@yandex.ru>
2022-10-08 12:10:41 -0700

    Initial Commit

---
+++ AppleMusicMVVM/AppleMusicMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicMVVMApp.swift
+//  AppleMusicMVVM
+//
+//  Created by TestTest on 08.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
